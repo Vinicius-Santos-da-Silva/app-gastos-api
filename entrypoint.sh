15371aeff9609daa5c74d9dcc14d558bee7620be
Vinicius Santos da Silva <vinicius.unisinos@hotmail.com>
2021-07-14 20:22:41 -0300

    ext-intl

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 chmod +x env
 cp env .env
+rm -rf composer.lock
 composer self-update
 composer update
 composer install
+composer require codeigniter4/framework --prefer-source
 composer fund
